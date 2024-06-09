8b61dbe2c532e8a9e0dd15b877c1a7e36330afd5
gh_exp <gh007a@yahoo.com>
2024-06-09 04:54:03 +0300

    done Project05

--- cypress/e2e/step-definitions/Project05.js
+++ cypress/e2e/step-definitions/Project05.js
@@ -1,4 +1,4 @@
-const { Given } = require('@badeball/cypress-cucumber-preprocessor')
+const { Given, Then, When } = require('@badeball/cypress-cucumber-preprocessor')
 
 const Project05 = require('../../pages/project05')
 
@@ -9,13 +9,30 @@ Given(/^the user is on "([^"]*)"$/, (url) => {
 })
 
 Then(/^the user should see the "([^"]*)" heading$/, (title) => {
-  proj.validateHeading(title).should('be.visible')
+  proj.validatePageHeadElVisible(title)
 })
 
-// Then(/^the user should see the "([^"]*)" heading$/, (title) => {
-//   proj.validateSubHeading(title)
-// })
+Then(/^the user should see the "([^"]*)" paragraph$/, (text) => {
+  proj.validatePHeading(text)
+})
 
-// Then(/^the user should see the "([^"]*)" paragraph$/, (text) => {
-//   proj.validatePHeading(text)
-// })
+Then(/^the user should see the "([^"]*)" button is disabled$/, (label) => {
+  proj.choiceButton(label).should('be.disabled')
+})
+
+Then(/^the user should see the "([^"]*)" button is enabled$/, (label) => {
+  proj.choiceButton(label).should('be.enabled')
+})
+
+When(/^the user clicks on the "([^"]*)" button$/, (label) => {
+  proj.choiceButton(label).click()
+})
+
+When(/^the user clicks on the "([^"]*)" button till it becomes disabled$/, (label) => {
+  proj.clickButtonTillitDisabled(label)
+})
+
+Then(/^the user should see "([^"]*)" City with the info below and an image$/, (cityName, dataTable) => {
+  proj.verifyCityInfo(dataTable)
+  proj.verifyCityImage(cityName)
+})
--- cypress/pages/project05.js
+++ cypress/pages/project05.js
@@ -1,27 +1,68 @@
 class Project05 {
   /* Locators */
-  getHeading() {
-    return cy.get('.is-size-3')
-  }
-  getSubHeading() {
-    return cy.get('#sub_heading')
+
+  getPage() {
+    return cy.get('.has-background-white-ter')
   }
 
   getPHeading() {
     return cy.get('#content')
   }
 
+  getPreviusButon() {
+    return cy.get('#previous')
+  }
+
+  getNextButon() {
+    return cy.get('#next')
+  }
+
+  getImage() {
+    return cy.get('.city_image')
+  }
+
+  getInfo() {
+    return cy.get('.Pagination_pagBodyData__vG6oj > p')
+  }
+
   /* Methods */
-  validateHeading(title) {
-    return cy.contains(title)
+
+  validatePageHeadElVisible(title) {
+    return this.getPage().contains(title).should('be.visible')
   }
 
-  validateSubHeading(title) {
-    this.getSubHeading().should('have.text', title)
+  validatePHeading() {
+    this.getPHeading().should('be.visible')
   }
 
-  validatePHeading(text) {
-    this.getPHeading().should('have.text', text)
+  choiceButton(label) {
+    return label === 'Previous' ? this.getPreviusButon() : this.getNextButon()
+  }
+
+  clickButtonTillitDisabled(label) {
+    this.choiceButton(label).then(($button) => {
+      function clickUntilDisabled() {
+        if (!$button.is(':disabled')) {
+          cy.wrap($button).click()
+          cy.wait(100).then(() => clickUntilDisabled())
+        }
+      }
+      clickUntilDisabled()
+    })
+  }
+
+  verifyCityInfo(dataTable) {
+    const arr = dataTable.rawTable.flat()
+
+    this.getInfo().each(($el, index) => {
+      cy.log(arr[index])
+      cy.wrap($el).should('exist').and('contain', arr[index])
+    })
+  }
+
+  verifyCityImage(cityName) {
+    this.getImage().should('exist').and('have.attr', 'alt', cityName)
   }
 }
+
 module.exports = Project05
